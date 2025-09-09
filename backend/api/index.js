cfe8ffb3e6206a7f9f1e6a4da875f67741ab9086
Muhammad Nihal <muhammadnihal9421@gmail.com>
2025-09-09 17:42:12 +0500

    port add in env file

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { supabase } from "../supabaseClient.js";
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
@@ -51,4 +52,6 @@ app.delete("/todos/:id", async (req, res) => {
 });
 
 // Run server
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
